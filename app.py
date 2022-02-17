1d89d25ac04a845e9a95a5bd46b1d218f8368ded
agungr23 <43489268+agungr23@users.noreply.github.com>
2022-02-18 02:02:42 +0700

    Update app.py

--- app.py
+++ app.py
@@ -620,7 +620,7 @@ def maphasil():
 #----------------------------------------------------------------------------
 
 if __name__ == '__main__':
-   port = int(os.environ.get("PORT", 5000))
+   port = int(os.getenv('PORT'))
    application.run(host='0.0.0.0', port=port)
    application.run(debug=True)
 
